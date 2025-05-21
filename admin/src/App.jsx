236734d3963cfa1bc472c49484bdbc118098488e
ronak nayak <nayakronak72@gmail.com>
2025-05-21 19:50:04 +0530

    changes done

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url="http://localhost:4000"
+  const url="https://gocuisine5.onrender.com"
 
   return (
     <div>
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const storeContextProvider = (props) => {
 
   const [cartItems, setCartItems] = useState({});
-  const url="http://localhost:4000";
+  const url="https://gocuisine4.onrender.com";
   const [token,setToken]=useState("");
   const [food_list,setFoodList]=useState([])
 
